6ba6da28fa9d2d69ca9cef2ad94f244416112aec
Dmitry Kirsanov <dmitkirsanov@yandex.ru>
2022-04-19 17:53:29 +0300

    sprint_06_final_04

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -12,6 +12,9 @@ urlpatterns = [
 ]
 
 handler404 = 'core.views.page_not_found'
+handler403 = 'core.views.csrf_failure'
+handler403 = 'core.views.permission_denied'
+handler500 = 'core.views.server_error'
 
 if settings.DEBUG:
     urlpatterns += static(
